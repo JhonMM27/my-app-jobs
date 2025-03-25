ba1cedc5e21af6de63d76e8e0c3273dc9991e241
JhonMM <mesonesjhon27@gmail.com>
2025-03-25 00:56:23 -0500

    Se Implementa La navegacion y las vistas Splash y Home

---
+++ src/screens/HomeScreem.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const HomeScreem = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text style={{color: '#FF5733', fontSize: 30}}>A dangello lo hacen Cachudos todas</Text>
+        </View>
+
+    )
+}
+
+export default HomeScreem
